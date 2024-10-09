4532962bf3005d6bde0fb23f878dc0505c994ba6
Anderson dos Santos Bauer <andersondossantosbauer@gmail.com>
2024-10-08 21:53:57 -0300

    Initial config

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Responses\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ClienteController extends Controller
+{
+    public function listar()
+    {
+        $customers = Cliente::all();
+        return ApiResponse::success('Lista de clientes', $customers);
+    }
+    public function listarPeloId(int $id)
+    {
+        $customers = Cliente::findOrFail($id);
+        return ApiResponse::success('Cliente informado', $customers);
+    }
+    public function salvar(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:clientes|max:200',
+        ]);
+
+        if ($validator->fails()) {
+            return ApiResponse::error('Erro de validação',
+             $validator->errors());
+        }
+
+        $customer = Cliente::create($request->all());
+        return ApiResponse::success('Salvo com sucesso!',
+         $customer);
+    }
+    public function editar(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required|string|max:255',
+            'email' => 'required|string|email|max:200',
+        ]);
+
+        if ($validator->fails()) {
+            return ApiResponse::error('Erro de validação!',
+             $validator->errors());
+        }
+
+        $customer = Cliente::findOrFail($id);
+        $customer->update($request->all());
+        return ApiResponse::success('Salvo com sucesso!',
+         $customer);
+    }
+    public function deletar(int $id)
+    {
+        $customer = Cliente::findOrFail($id);
+        $customer->delete();
+        return ApiResponse::success('Cliente deletado com sucesso!');
+    }
+}
